c61de057ce31dc220ac8e5e1eedf8a5cfafc720b
Roman Boytsov <domovon@outlook.com>
2023-05-23 17:06:31 +0300

    Delete souf

--- src/main/java/ru/netology/services/CountRestService.java
+++ src/main/java/ru/netology/services/CountRestService.java
@@ -8,17 +8,11 @@ public class CountRestService {
         for (int month = 1; month <= 12; month++) {
             if (money >= threshold) { // можем ли отдыхать?
                 count++; // увеличиваем счётчик месяцев отдыха
-                int currentMoney = money;
-                System.out.printf("Месяц " + month + ". Денег " + money + ". Буду отдыхать. Потратил -" + expenses);
                 money = (money - expenses) / 3;
-                System.out.printf(", затем еще -" + (currentMoney - expenses - money) + "\n");
             } else {
-                System.out.printf("Месяц " + month + ". Денег " + money + ". Буду работать. Заработал +" + income);
                 money = money + income - expenses;
-                System.out.printf(", потратил -" + expenses + "\n");
             }
         }
-        System.out.printf("\n");
         return count;
     }
 }
