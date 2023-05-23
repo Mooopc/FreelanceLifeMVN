69a9408c47778e25dc92a88f11af250e8885c2e0
Roman Boytsov <domovon@outlook.com>
2023-05-23 17:56:06 +0300

    Parametrized tests

--- src/test/java/ru/netology/services/CountRestServiceTest.java
+++ src/test/java/ru/netology/services/CountRestServiceTest.java
@@ -1,33 +1,17 @@
 package ru.netology.services;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class CountRestServiceTest {
-    @Test
-    public void testLowExpences() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/restMonths.csv")
+    public void testLowExpences(int expected, int threshold, int income, int expenses) {
         CountRestService service = new CountRestService();
 
-        int threshold = 150_000;
-        int income = 100_000;
-        int expenses = 60_000;
-        int expected = 2;
         int actual = service.calculate(threshold, income, expenses);
 
         Assertions.assertEquals(expected, actual);
     }
-
-    @Test
-    public void testHighExpences() {
-        CountRestService service = new CountRestService();
-
-        int threshold = 20_000;
-        int income = 10_000;
-        int expenses = 3_000;
-        int expected = 3;
-        int actual = service.calculate(threshold, income, expenses);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
 }
